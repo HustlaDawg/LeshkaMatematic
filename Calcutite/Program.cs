9b86307a3e532882c815ea4ef2a421b058a967fb
HustlaDawgg <kierlog79@icloud.com>
2021-02-01 17:00:27 +0700

    Добавлен +

--- Calcutite/Program.cs
+++ Calcutite/Program.cs
@@ -10,6 +10,11 @@ namespace Calcutite
     {
         static void Main(string[] args)
         {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
         }
     }
 }
